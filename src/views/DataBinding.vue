3d52c2927ddeb184a0c4f54158c3fa75616edcd3
wjdgus1122 <qodqodantk@gmail.com>
2022-11-04 23:52:41 +0900

    radio button

--- src/views/DataBinding.vue
+++ src/views/DataBinding.vue
@@ -1,19 +1,33 @@
 <template>
     <div>
-        <label><input type="checkbox" value="서울" v-model="checked">서울</label>
+        <!-- <label><input type="checkbox" value="서울" v-model="checked">서울</label>
         <label><input type="checkbox" value="부산" v-model="checked">부산</label>
         <label><input type="checkbox" value="제주" v-model="checked">제주</label>
         <br>
-        <span>체크한 지역: {{checked}}</span>
+        <span>체크한 지역: {{checked}}</span> -->
+        <label><input type="radio" v-bind:value="radioValue1" v-model="picked">서울</label>
+        <label><input type="radio" v-bind:value="radioValue2" v-model="picked2">부산</label>
+        <label><input type="radio" v-bind:value="radioValue3" v-model="picked">제주</label>
+        <br>
+        <span>선택한지역 : {{picked}} {{picked2}}</span>
     </div>
 </template>
 
 <script>
     export default {
-        data() {
+        // data() {
+        //     return {
+        //         checked : []
+        //     };
+        // }
+        data(){
             return {
-                checked : []
-            };
+                picked : '',
+                picked2 : '',
+                radioValue1: `서울`,
+                radioValue2: `부산`,
+                radioValue3: `제주`
+            }
         }
     }
 </script>
